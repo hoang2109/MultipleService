e329e7bb661a7c7df4d4b35190b6026f22874838
Hoang Nguyen <hoang21091989@gmail.com>
2021-12-19 14:27:40 +0800

    Create empty project

---
+++ MultipleService/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  MultipleService
+//
+//  Created by Hoang Nguyen on 19/12/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ MultipleServiceTests/MultipleServiceTests.swift
@@ -0,0 +1,17 @@
+//
+//  MultipleServiceTests.swift
+//  MultipleServiceTests
+//
+//  Created by Hoang Nguyen on 19/12/21.
+//
+
+import XCTest
+@testable import MultipleService
+
+class MultipleServiceTests: XCTestCase {
+
+    func test() {
+        
+    }
+
+}
