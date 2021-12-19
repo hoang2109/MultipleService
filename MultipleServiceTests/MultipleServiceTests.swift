31135447a7d6a99cea055c6c21f83876575f5bea
Hoang Nguyen <hoang21091989@gmail.com>
2021-12-19 14:58:24 +0800

    Initial `MainLoadAdapter`

--- MultipleServiceTests/MultipleServiceTests.swift
+++ MultipleServiceTests/MultipleServiceTests.swift
@@ -8,10 +8,79 @@
 import XCTest
 @testable import MultipleService
 
-class MultipleServiceTests: XCTestCase {
 
-    func test() {
+protocol ServiceA {
+    typealias Result = Swift.Result<String, Error>
+    
+    func loadServiceA(completion: (Result) -> Void)
+}
+
+protocol ServiceB {
+    typealias Result = Swift.Result<String, Error>
+    
+    func loadServiceB(completion: (Result) -> Void)
+}
+
+protocol ServiceC {
+    typealias Result = Swift.Result<String, Error>
+    
+    func loadServiceC(completion: (Result) -> Void)
+}
+
+struct MainData: Equatable {
+    let dataA: String
+    let dataB: String
+    let dataC: String
+}
+
+protocol MainService {
+    typealias Result = Swift.Result<MainData, Error>
+    
+    func load(completion: (Result) -> Void)
+}
+
+class MainLoadAdapter: MainService {
+    
+    private let serviceA: ServiceA
+    private let serviceB: ServiceB
+    private let serviceC: ServiceC
+    
+    init(serviceA: ServiceA, serviceB: ServiceB, serviceC: ServiceC) {
+        self.serviceA = serviceA
+        self.serviceB = serviceB
+        self.serviceC = serviceC
+    }
+    
+    func load(completion: (MainService.Result) -> Void) {
         
     }
+}
 
+
+class MultipleServiceTests: XCTestCase {
+
+    func test_canInit() {
+        let loader = LoaderStub()
+        let sut = MainLoadAdapter(serviceA: loader, serviceB: loader, serviceC: loader)
+        XCTAssertNotNil(sut)
+    }
+    
+    // MARK: - Helpers
+
+    class LoaderStub: ServiceA, ServiceB, ServiceC {
+        var resultAStub: ServiceA.Result = .success("")
+        func loadServiceA(completion: (ServiceA.Result) -> Void) {
+            completion(resultAStub)
+        }
+        
+        var resultBStub: ServiceA.Result = .success("")
+        func loadServiceB(completion: (ServiceB.Result) -> Void) {
+            completion(resultBStub)
+        }
+        
+        var resultCStub: ServiceA.Result = .success("")
+        func loadServiceC(completion: (ServiceC.Result) -> Void) {
+            completion(resultCStub)
+        }
+    }
 }
